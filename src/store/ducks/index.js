d0459af2f7df4dad778144970db7abbd3e946dba
Paulo <phacff@gmail.com>
2021-02-19 18:04:54 -0300

    feat: redux concluded

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,25 +1,37 @@
-import axios from 'axios'
+import axios from "axios";
 
 const ActionsType = {
   GET_PRODUCTS: "@shop/GET_PRODUCTS",
+  GET_DISCOUNT: "@shop/GET_DISCOUNT",
 };
 
 const Actions = {
-    getProducts: (products)=>({
-        type: ActionsType.GET_PRODUCTS,
-        products
-    })
+  getProducts: (products) => ({
+    type: ActionsType.GET_PRODUCTS,
+    products,
+  }),
+  getDiscounts: (discounts) => ({
+    type: ActionsType.GET_DISCOUNT,
+    discounts,
+  }),
 };
 
 const reducer_state = {
   products: {},
+  discounts: {},
 };
 
 const reducer = (state = reducer_state, actions) => {
   switch (ActionsType) {
     case ActionsType.GET_PRODUCTS:
-        reducer_state.products =  Actions.getProducts
-      return state
+      const { products } = actions;
+      reducer_state.products = products;
+      return state;
+
+    case ActionsType.GET_DISCOUNT:
+      const { discounts } = actions;
+      reducer_state.discounts = discounts;
+      return state;
 
     default:
       return state;
@@ -28,8 +40,14 @@ const reducer = (state = reducer_state, actions) => {
 export default reducer;
 
 export const Thunk = {
-    getProducts: (url)=>(dispatch)=>{
-        axios(url)
-        .then((res)=> console.log(res))
-    }
+  getProducts: () => (dispatch) => {
+    axios(
+      "https://shielded-wildwood-82973.herokuapp.com/products.json"
+    ).then((res) => console.log(res));
+  },
+  getDiscounts: () => (dispatch) => {
+    axios(
+      "https://shielded-wildwood-82973.herokuapp.com/vouchers.json"
+    ).then((res) => console.log(res));
+  },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,2 +1,11 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import Reducer from "./ducks";
+
+const reducers = combineReducers({
+  reducer: Reducer,
+});
+
+const store = createStore(reducers, applyMiddleware(thunk))
+
+export default store
